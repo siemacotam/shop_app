ac795b963503a6e8cf353f319138625bd8a033b6
Grzegorz Celek <g.celek@gmail.com>
2021-04-15 15:20:39 +0200

    work in progress

--- src/layouts/Banner.js
+++ src/layouts/Banner.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import '../styles/Banner.css'
 
+const data = [
+    '../images/1.jpg',
+    '../images/2.jpg',
+    '../images/3.jpg',
+    '../images/4.jpg'
+]
+
 class Banner extends Component {
-    state = {  }
+    index = 0;
+
+    state = { 
+        pic: data[this.index]
+     }
+
+     componentDidMount(){
+         setInterval(()=>{
+            this.setState({
+                pic:data[this.index]
+            })
+            if(this.index === data.length -1){
+                this.index = -1
+            }
+            this.index ++
+         },5000)
+         
+        }
+
     render() { 
         return ( 
-            <div className='banner'>siema banner</div>
+            <div className='banner'>
+                <div className='baner'><img src={this.state.pic} alt=""/> siema banner</div>
+            </div>
          );
     }
 }
